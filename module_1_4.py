80bafae6ca4526cfab3070dd03e94c7dd20dcc37
Aleksandr0917 <180825344+Aleksandr0917@users.noreply.github.com>
2024-09-24 19:44:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Привет! Как тебя зовут: ")
+print(len(my_string))
+my_string = ("Привет! Как тебя зовут: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-3])
